16b1426b71c81ef25db790a4455bd0e96cf61ef0
mnod <mnod@example.com>
2019-07-14 18:13:29 +0900

    add HTTP_X_FORWARDED_FOR for load balancer

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo $_SERVER["REMOTE_ADDR"] ;
+$headeraddr = $_SERVER["HTTP_X_FORWARDED_FOR"];
+$remoteaddr = $_SERVER["REMOTE_ADDR"];
+if ( $headeraddr == "" ) {
+  echo $remoteaddr;
+}
+else {
+  echo $headeraddr;
+}
 ?>
